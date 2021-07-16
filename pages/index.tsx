e2744aaf3130aed69ad56e81bab6b06ecede67e7
Gabriel <knuckles4x@gmail.com>
2021-07-16 09:01:41 -0300

    generate new image with puppeteer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div>
+      <h1>Gerador de Cancelamento Alpha</h1>
+
+      <div>
+        <img
+          src="/api/image-generator?titlte=Hello World"
+          alt="Imagem gereada"
+        />
+
+        <a href="/api/image-generator?titlte=Hello World">
+          /api/image-generator?titlte=Hello World
+        </a>
+      </div>
+    </div>
+  );
 }
